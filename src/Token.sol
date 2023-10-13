867e08373096a75277dd48a4c9bd1a91ef7da99e
Yves Boutellier <63062257+yvesbou@users.noreply.github.com>
2023-10-13 21:29:55 +0200

    ready to start

--- src/Token.sol
+++ src/Token.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-// import {ERC20} from "solmate/tokens/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DAOToken is ERC20 {
